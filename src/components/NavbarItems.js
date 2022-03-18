1b80928fb2905610672ff1afdc302439821c669b
Brian Baez <brianbaez.dev@gmail.com>
2022-03-18 16:55:13 -0500

    Navbar items in their own file

---
+++ src/components/NavbarItems.js
@@ -0,0 +1,22 @@
+export const navItems = [
+  {
+    id: "1",
+    name: "People"
+  },
+  {
+    id: "2",
+    name: "Starships"
+  },
+  {
+    id: "3",
+    name: "Vehicles"
+  },
+  {
+    id: "4",
+    name: "Species"
+  },
+  {
+    id: "5",
+    name: "Planets"
+  }
+];
