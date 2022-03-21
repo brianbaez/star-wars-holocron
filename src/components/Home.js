fe7af17ade96f66f9697f2928cef546fdd2c6e79
Brian Baez <brianbaez.dev@gmail.com>
2022-03-21 17:06:34 -0500

    Updated Home component with content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
 import React from "react";
 
+const holocronImg = require("../assets/images/holocrons.png");
+
 function Home() {
   return (
-    <div className="container">
-      <h1>Home Page</h1>
+    <div className="d-flex flex-column align-items-center mt-3">
+
+      <div className="container d-flex justify-content-center pb-3">
+        <img className="img-fluid" src={holocronImg} alt="holocrons"></img>
+      </div>
+
+      <div className="container">
+        <h1 className="text-center">Star Wars Holocron</h1>
+        <p className="text-center">
+          holocrons in the star wars universe are information-storage devices used by both the jedi order and the sith.
+          they contained ancient lessons and valuable information in holographic form.
+        </p>
+      </div>
+
+      <div className="container">
+        <h1 className="text-center">Choose Your Side</h1>
+      </div>
+
+      <div className="container d-flex justify-content-center">
+        <button className="btn btn-outline-info me-5">Light Side</button>
+        <button className="btn btn-outline-danger">Dark Side</button>
+      </div>
     </div>
   );
 }
